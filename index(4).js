283228d1df9f7b4dceb4b9969fcaa2f319935ce8
Ethan Vesely <Evesel2592west-mec.org>
2020-10-28 18:19:41 -0700

    added files

---
+++ index(4).js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+        x.type = "text";
+    }else{
+        x.type="password";
+    }
+//This function allows for the user to show what they have typed into the password box by clicking on a checkbox. It can be toggled on and off. 
+}
+
+var input=""
